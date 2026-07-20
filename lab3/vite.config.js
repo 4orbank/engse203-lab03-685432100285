5ecc3902518001e9224c37e31c04c00b5696f70e
Bank <tanakhanluenam@gmail.com>
2026-07-20 12:25:34 +0700

    feat(lab3): complete campus service request

--- lab3/vite.config.js
+++ lab3/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/engse203-lab03-685432100285/lab3/',
+  base: '/engse203-lab03-STUDENT-ID/',
   build: {
-    outDir: '../docs/lab3',
+    outDir: 'docs',
     emptyOutDir: true,
   },
 });
